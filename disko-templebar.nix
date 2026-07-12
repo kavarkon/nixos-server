5f5751ec182d4b1175d020da526f7434af835358
Ioanna Fomina <fomina-ioanna@rambler.ru>
2026-07-12 17:01:07 +0300

    update configs for templebar

---
+++ disko-templebar.nix
@@ -0,0 +1,24 @@
+{
+  disko.devices.disk.main = {
+    device = "/dev/sda";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          name = "boot";
+          size = "1M";
+          type = "EF02";
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
+    };
+  };
+}
